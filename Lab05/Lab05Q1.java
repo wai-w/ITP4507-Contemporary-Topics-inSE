8666dc77389ba0526c7413e44b1f96bb731f548a
Chan Ying Wai <240336478@stu.vtc.edu.hk>
2025-10-23 03:59:57 +0000

    ts

--- Lab05/Lab05Q1.java
+++ Lab05/Lab05Q1.java
@@ -1,32 +1,51 @@
+
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Lab05Q1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        Command cmd1 = new Command1(sc);
-        Command cmd2 = new Command2(sc);
 
-        System.out.println("Enter command: 0 = exit, 1 = undo, 2 = Command1, 3 = Command2");
+    public static Scanner sc = new Scanner(System.in);
 
-        int command = sc.nextInt();
-        switch (command) {
-            case 0:
-                System.out.println("Exiting...");
-                break;
-            case 1:
-                cmd1.undo();
-                cmd2.undo();
-                break;
-            case 2:
-                cmd1.execute();
-                break;
-            case 3:
-                cmd2.execute();
-                break;
-            default:
-                System.out.println("Invalid command.");
+    public static void main(String[] args) {
+
+        int amount = 0;
+        Command com; //Object for a command
+        Stack commandStack = new Stack();
+        int command;
+        while (true) {
+            System.out.println("Enter command: 0 = exit, 1 = undo, 2 = Command1, 3 = Command2");
+            command = sc.nextInt();
+			
+            switch (command) {
+                case 0:
+                    System.exit(0);
+                    break;
+
+                case 1:
+                    // undo the commands	
+                    if (!commandStack.empty()) {
+                        // get the latest command object in the stack 
+                        Command c = (Command) commandStack.pop();
+                        // undo the latest command					
+                        c.undo();
+                    } else {
+                        System.out.println("Nothing to undo!");
+                    }
+                    break;
+
+                case 2:
+                    //must be a new object!
+                    com = new Command1(sc);
+                    com.execute();
+                    commandStack.push(com);
+                    break;
+
+                case 3:
+                    com = new Command2(sc);
+                    com.execute();
+                    commandStack.push(com);
+                    break;
+            }
         }
-
-        sc.close();
     }
 }
