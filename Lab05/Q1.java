42209113826d923c29ac0a03a2a4de7915bd2a9b
Chan Ying Wai <240336478@stu.vtc.edu.hk>
2025-10-23 03:38:28 +0000

    g

--- Lab05/Q1.java
+++ Lab05/Q1.java
@@ -4,7 +4,38 @@ public interface Command{
 
 }
 
-public Command1 implements Command{
+public class Command1 implements Command{
     private int id;
-    Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
+
+    public Command1(Scanner sc){
+        this.sc = sc;
+    }
+    public void execute(){
+        System.out.println("Input ID for Command 1:");
+        id = sc.nextInt();  
+        System.out.println(id + "Command 1 : execute()");
+    }
+    public void undo(){
+        System.out.println(id + "Command 1 : undo()");
+    }
 }
+
+
+public class Command2 implements Command{
+    private int id;
+    private static final Scanner sc = new Scanner(System.in);
+
+    public Command2(Scanner sc){
+        this.sc = sc;
+    }
+    public void execute(){
+        System.out.println("Input ID for Command 2:");
+        id = sc.nextInt();  
+        System.out.println(id + "Command 2 : execute()");
+    }
+    public void undo(){
+        System.out.println(id + "Command 2 : undo()");
+    }
+}
+
