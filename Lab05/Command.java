f9b3ca886d70d5e781c932d582412187148f204a
Chan Ying Wai <240336478@stu.vtc.edu.hk>
2025-10-23 03:49:08 +0000

    asdasd

--- Lab05/Command.java
+++ Lab05/Command.java
@@ -1,43 +1,46 @@
-public interface Command{
+import java.util.Scanner;
+
+public interface Command {
     public void execute();
+
     public void undo();
 
 }
 
-class Command1 implements Command{
+class Command1 implements Command {
     private int id;
-    private static final Scanner sc = new Scanner(System.in);
+    Scanner sc;
 
-    public Command1(Scanner sc){
+    public Command1(Scanner sc) {
         this.sc = sc;
     }
-    public void execute(){
+
+    public void execute() {
         System.out.println("Input ID for Command 1:");
-        id = sc.nextInt();  
+        id = sc.nextInt();
         System.out.println(id + "Command 1 : execute()");
     }
-    public void undo(){
+
+    public void undo() {
         System.out.println(id + "Command 1 : undo()");
     }
 }
 
-
-
-class Command2 implements Command{
+class Command2 implements Command {
     private int id;
-    private static final Scanner sc = new Scanner(System.in);
+    Scanner sc;
 
-    public Command2(Scanner sc){
+    public Command2(Scanner sc) {
         this.sc = sc;
     }
-    public void execute(){
+
+    public void execute() {
         System.out.println("Input ID for Command 2:");
-        id = sc.nextInt();  
+        id = sc.nextInt();
         System.out.println(id + "Command 2 : execute()");
     }
-    public void undo(){
+
+    public void undo() {
         System.out.println(id + "Command 2 : undo()");
     }
 }
-
-
--- Lab05/Lab05Q1.java
+++ Lab05/Lab05Q1.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lab05Q1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
